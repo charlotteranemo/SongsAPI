55d66cba47a54d5e873e4658f252871c3101f557
Charlotte Ranemo <info@charlotteranemo.se>
2021-02-15 11:51:56 +0100

    Add project files.

---
+++ SongsAPI/Data/SongContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SongsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsAPI.Data
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Songs> Songs { get; set; }
+    }
+}
---
+++ SongsAPI/Migrations/20210215092117_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SongsAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Artist = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Length = table.Column<int>(nullable: false),
+                    Category = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
